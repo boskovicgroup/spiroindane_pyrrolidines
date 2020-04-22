f6eb07769a5ffe6fb337c115d59eaee647d37592
Zarko Boskovic <zarko@Zarkos-MacBook-Air.local>
2020-04-22 11:31:54 -0500

    Adding new R script

--- scripts/cp_analysis.R
+++ scripts/cp_analysis.R
@@ -1,5 +1,11 @@
+library(plyr)
+# control names
+names = c( "etoposide", "berberine HCl", "CA074 Me ester", "SB203580", "rapamycin", 
+           "taxol", "latrunculine B", "cytochalasine B", "rotenone", "NPPB", 
+           "tetrandrine","vincristine", "fenbendazole")
+
 # Load the controls (3 replicates)
-ctrl1 = read.csv('Documents/zarkolab/publications/bmc_spiroindane_pyrrolidines/Ctrl1.csv')
+ctrl1 = read.csv('Ctrl1.csv')
 ctrl1$Name <- mapvalues(ctrl1$Molecule, from=unique(ctrl1$Molecule), names)
 rownames(ctrl1) = paste(ctrl1$Name, round(ctrl1$Concentration,4))
 nums <- unlist(lapply(ctrl1, is.numeric))  
@@ -7,14 +13,14 @@ ctrl1.num = ctrl1[,nums]
 ctrl1.num.scaled <- scale(ctrl1.num)
 ctrl1.cor = cor(t(ctrl1.num.scaled), use = 'pairwise.complete.obs')
 
-ctrl2 = read.csv('Documents/zarkolab/publications/bmc_spiroindane_pyrrolidines/Ctrl2.csv')
+ctrl2 = read.csv('Ctrl2.csv')
 rownames(ctrl2) = paste(ctrl2$Molecule, round(ctrl2$Concentration, 5))
 nums <- unlist(lapply(ctrl2, is.numeric))  
 ctrl2.num = ctrl2[,nums]
 ctrl2.num.scaled <- scale(ctrl2.num)
 ctrl2.cor = cor(t(ctrl2.num.scaled), use = 'pairwise.complete.obs')
 
-ctrl3 = read.csv('Documents/zarkolab/publications/bmc_spiroindane_pyrrolidines/Ctrl3.csv')
+ctrl3 = read.csv('Ctrl3.csv')
 rownames(ctrl3) = paste(ctrl3$Molecule, round(ctrl3$Concentration, 5))
 nums <- unlist(lapply(ctrl3, is.numeric))  
 ctrl3.num = ctrl3[,nums]
@@ -22,21 +28,24 @@ ctrl3.num.scaled <- scale(ctrl3.num)
 ctrl3.cor = cor(t(ctrl3.num.scaled), use = 'pairwise.complete.obs')
 
 # Load the test compounds (3 replicates)
-rep1 = read.csv('Documents/zarkolab/publications/bmc_spiroindane_pyrrolidines/Rep1.csv')
+rep1 = read.csv('Rep1.csv')
+rep1$Name = rep1$Molecule
 rownames(rep1) = paste(rep1$Molecule, round(rep1$Concentration, 5))
 nums <- unlist(lapply(rep1, is.numeric))  
 rep1.num = rep1[,nums]
 rep1.num.scaled <- scale(rep1.num)
 rep1.cor = cor(t(rep1.num), use = 'pairwise.complete.obs')
 
-rep2 = read.csv('Documents/zarkolab/publications/bmc_spiroindane_pyrrolidines/Rep2.csv')
+rep2 = read.csv('Rep2.csv')
+rep2$Name = rep2$Molecule
 rownames(rep2) = paste(rep2$Molecule, round(rep2$Concentration, 5))
 nums <- unlist(lapply(rep2, is.numeric))  
 rep2.num = rep2[,nums]
 rep2.num.scaled <- scale(rep2.num)
 rep2.cor = cor(t(rep2.num.scaled), use = 'pairwise.complete.obs')
 
-rep3 = read.csv('Documents/zarkolab/publications/bmc_spiroindane_pyrrolidines/Rep3.csv')
+rep3 = read.csv('Rep3.csv')
+rep3$Name = rep3$Molecule
 rownames(rep3) = paste(rep3$Molecule, round(rep3$Concentration, 5))
 nums <- unlist(lapply(rep3, is.numeric))  
 rep3.num = rep3[,nums]
@@ -44,6 +53,7 @@ rep3.num.scaled <- scale(rep3.num)
 rep3.cor = cor(t(rep3.num.scaled), use = 'pairwise.complete.obs')
 
 # Aggregate all concentrations of individual compounds
+rep3.agg = aggregate(rep3, list(rep3$Molecule),mean)
 rownames(rep3.agg) = rep3.agg$Group.1
 nums <- unlist(lapply(rep3.agg, is.numeric))
 rep3.agg.num = rep3.agg[,nums]
@@ -86,24 +96,22 @@ ctrl3.agg.num.scaled <- scale(ctrl3.agg.num)
 ctrl3.agg.cor = cor(t(ctrl3.agg.num.scaled), use ='pairwise.complete.obs')
 
 # Extract fingerprints of active compounds from replicate 1 of test compounds
-actives = rep1[rep1$Molecule == 'XK00001384-001' & rep1$Concentration == 50, ]
-rbind(actives, rep1[rep1$Molecule == 'XK00001401-001' & rep1$Concentration == 50, ])
-rbind(actives, rep1[rep1$Molecule == 'XK00001401-001' & rep1$Concentration == 50, ])
+actives = list()
+actives = rbind(actives, rep1[rep1$Molecule == 'XK00001384-001' & rep1$Concentration == 50, ])
 actives = rbind(actives, rep1[rep1$Molecule == 'XK00001401-001' & rep1$Concentration == 50, ])
 actives = rbind(actives, rep1[rep1$Molecule == 'XK00001401-001' & rep1$Concentration == 16.67, ])
 actives = rbind(actives, rep1[rep1$Molecule == 'XK00001401-001' & rep1$Concentration == 5.560, ])
 actives = rbind(actives, rep1[rep1$Molecule == 'XK00001387-001' & rep1$Concentration == 50, ])
 actives = rbind(actives, rep1[rep1$Molecule == 'XK00001386-001' & rep1$Concentration == 50, ])
 
-# Extract fingerprints of active compounds from replicate 1 of test compounds
-actives = rep2[rep2$Molecule == 'XK00001384-001' & rep2$Concentration == 50, ]
-rbind(actives, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 50, ])
-rbind(actives, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 50, ])
-actives = rbind(actives, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 50, ])
-actives = rbind(actives, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 16.67, ])
-actives = rbind(actives, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 5.560, ])
-actives = rbind(actives, rep2[rep2$Molecule == 'XK00001387-001' & rep2$Concentration == 50, ])
-actives = rbind(actives, rep2[rep2$Molecule == 'XK00001386-001' & rep2$Concentration == 50, ])
+# Extract fingerprints of active compounds from replicate 2 of test compounds
+actives2 = list()
+actives2 = rbind(actives2, rep2[rep2$Molecule == 'XK00001384-001' & rep2$Concentration == 50, ])
+actives2 = rbind(actives2, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 50, ])
+actives2 = rbind(actives2, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 16.67, ])
+actives2 = rbind(actives2, rep2[rep2$Molecule == 'XK00001401-001' & rep2$Concentration == 5.560, ])
+actives2 = rbind(actives2, rep2[rep2$Molecule == 'XK00001387-001' & rep2$Concentration == 50, ])
+actives2 = rbind(actives2, rep2[rep2$Molecule == 'XK00001386-001' & rep2$Concentration == 50, ])
 
 
 #append actives to controls and cluster
@@ -116,7 +124,17 @@ map = setNames(unique(ctrl1$Molecule), names)
 
 
 
-ctrl1_plus_actives = rbind (ctrl3, actives)
+ctrl1_plus_actives2 = rbind (ctrl1, actives2)
+ctrl1_plus_actives2$Name = mapvalues(ctrl1_plus_actives2$Molecule, from=unique(ctrl1_plus_actives2$Molecule), to=names_with_actives)
+ctrl1_plus_actives2.agg = aggregate(ctrl1_plus_actives2, list(ctrl1_plus_actives2$Name),mean)
+rownames(ctrl1_plus_actives2.agg) = ctrl1_plus_actives2.agg$Group.1
+nums <- unlist(lapply(ctrl1_plus_actives2.agg, is.numeric))  
+ctrl1_plus_actives2.agg.num = ctrl1_plus_actives2.agg[,nums]
+ctrl1_plus_actives2.agg.num.scaled <- scale(ctrl1_plus_actives2.agg.num)
+ctrl1_plus_actives2.agg.cor = cor(t(ctrl1_plus_actives2.agg.num.scaled), use = 'pairwise.complete.obs')
+plot(hclust(1 - as.dist(ctrl1_plus_actives2.agg.cor)), main = 'Actives from rep2')
+
+ctrl1_plus_actives = rbind (ctrl1, actives)
 ctrl1_plus_actives$Name = mapvalues(ctrl1_plus_actives$Molecule, from=unique(ctrl1_plus_actives$Molecule), to=names_with_actives)
 ctrl1_plus_actives.agg = aggregate(ctrl1_plus_actives, list(ctrl1_plus_actives$Name),mean)
 rownames(ctrl1_plus_actives.agg) = ctrl1_plus_actives.agg$Group.1
@@ -124,6 +142,4 @@ nums <- unlist(lapply(ctrl1_plus_actives.agg, is.numeric))
 ctrl1_plus_actives.agg.num = ctrl1_plus_actives.agg[,nums]
 ctrl1_plus_actives.agg.num.scaled <- scale(ctrl1_plus_actives.agg.num)
 ctrl1_plus_actives.agg.cor = cor(t(ctrl1_plus_actives.agg.num.scaled), use = 'pairwise.complete.obs')
-plot(hclust(dist(ctrl1_plus_actives.agg.num)), main = 'Actives from rep2')
-
-
+plot(hclust(1 - as.dist(ctrl1_plus_actives.agg.cor)), main = 'Actives from rep1')
